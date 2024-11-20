907393c05f717bcc7b20e7a3bba011ae2bef7337
JuoG <116748990+Juoelenis@users.noreply.github.com>
2024-11-20 16:03:34 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+//import ur mom
+        document.addEventListener('DOMContentLoaded', function() {
+          if (typeof archive_analytics !== 'undefined')
+            archive_analytics.create_tracking_image('in_page_executes');
+        });
+      
